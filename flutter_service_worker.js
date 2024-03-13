db9c4c679697f12c24a347516c33ef392223a283
endor <zbcdady@gmail.com>
2024-03-13 22:43:39 +0800

    3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "1702a09f2d15a3f961da20e067edebe6",
-"index.html": "3c08287424493e6bb7bbb0f6b33ee43f",
-"/": "3c08287424493e6bb7bbb0f6b33ee43f",
-"main.dart.js": "62e748ff7c195b253313626ae16eafec",
+"index.html": "465a97261f3e4a2484fedb9588ec6fb8",
+"/": "465a97261f3e4a2484fedb9588ec6fb8",
+"main.dart.js": "d5a12464551fd110e1a611101678ef5a",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
